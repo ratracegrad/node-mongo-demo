71418d96958363e1afea202a0358636cb3297333
Jennifer Bland <ratracegrad@gmail.com>
2016-11-13 11:20:02 -0500

    Updated to use name istead of item

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("item saved to database");
+            res.send("Name saved to database");
         })
         .catch(err => {
-            res.status(400).send("unable to save to database");
+            res.status(400).send("Unable to save to database");
         });
 });
 
